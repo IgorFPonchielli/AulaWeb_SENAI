c243ac60d12013f8a0a5f7cd7845566c729f2209
Igor Ponchielli <42591808+IgorFPonchielli@users.noreply.github.com>
2021-03-27 19:57:42 -0300

    gif updates

--- gif-api/js/script.js
+++ gif-api/js/script.js
@@ -36,25 +36,20 @@ $(function () {
             params.tag = busca;          
         }
 
-        if ($(this).hasClass("btn-download")) {
-            console.log("clicou no download")
-        }
-
         $.ajax({
             url: url + $.param(params),
             method: "GET",
             beforeSend: function () {
-                $('#results').after('<p class="loading">Aguarde! Garregando... </p>')
+                $('#results').after('<p class="loading text-center">Aguarde! Garregando... </p>')
             },
             error: function () {
-                $('#results').after('<p class="loading">Deu ruim</p>')
+                $('#results').after('<p class="loading text-center">Falhou!</p>')                       
             },
             success: function (dados) {
                 console.log(url + $.param(params))
                 console.log(dados)
                 if(params.tag != undefined && params.q === undefined) {
                      randomGif(dados)
-                     console.log(dados)
                 } else {
                     mostrarGifs(dados, params);
                }            
@@ -65,42 +60,66 @@ $(function () {
         });
     });
 
-    function randomGif(dados) {          
-            $('#results').append(`
-            <div class="row justify-content-center">
-                <div class="col-md-6">
-                    <div class="card mb-12 box-shadow">
-                        <img src="${dados.data.images.fixed_height.url}"
-                            class="card-img-top">
-                        <div class="card-body">
-                            <p class="card-text">Rating: <b class="text-uppercase">${dados.data.rating}</b></p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">                                    
-                                <input type="button" class="btn btn-download btn-sm btn-outline-secondary" value="Download">                                    
-                                </div>
+    function randomGif(dados) {     
+        
+        var urlOriginal = "*";
+
+        if(dados.data.source != "") {
+            urlOriginal = dados.data.source
+        } else {
+            urlOriginal = dados.data.url
+        }
+
+        $('#results').append(`
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card mb-12 box-shadow">
+                    <img src="${dados.data.images.fixed_height.url}"
+                        class="card-img-top" alt="${dados.data.title}">
+                    <div class="card-body">
+                        <h6>${dados.data.title}</h6>
+                        <p class="card-text">Rating: <b class="text-uppercase">${dados.data.rating}</b></p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                            <a href="${urlOriginal}" target="_blank">                                    
+                                <input type="button" class="btn btn-sm btn-outline-secondary" value="Ir para conteudo original">
+                            </a>                                   
                             </div>
-                        </div>                        
-                    </div>
+                        </div>
+                    </div>                        
                 </div>
-            </div`)
+            </div>
+        </div`)
     }
         
 
-    function mostrarGifs(dados, params) {   
+    function mostrarGifs(dados, params) {  
+        
+        var urlOriginal = "*";
+        
         for (i = 0; i < params.limit; i++) { 
             if(i % 3 == 0) {
                 $('#results').append('<div class="row">');
             }
+            if(dados.data[i].source != "") {
+                urlOriginal = dados.data[i].source
+            } else {
+                urlOriginal = dados.data[i].url
+            }
+
             $('#results .row:last-child').append(`
             <div class="col-md-4">
                 <div class="card mb-4 box-shadow">
                     <img src="${dados.data[i].images.fixed_height.url}"
-                        class="card-img-top">
+                        class="card-img-top" alt="${dados.data[i].title}">
                     <div class="card-body">
+                        <h6>${dados.data[i].title}</h6>
                         <p class="card-text">Rating: <b class="text-uppercase">${dados.data[i].rating}</b></p>
                         <div class="d-flex justify-content-between align-items-center">
                             <div class="btn-group">
-                            <input type="button" class="btn btn-download btn-sm btn-outline-secondary" value="Download"> 
+                            <a href="${urlOriginal}" target="_blank">                                    
+                                    <input type="button" class="btn btn-sm btn-outline-secondary" value="Ir para conteudo original">
+                                </a>       
                             </div>
                         </div>
                     </div>                        
